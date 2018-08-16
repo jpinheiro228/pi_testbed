c0351d522d37b8a1aa5c715ad003db8142eb0407
Joao Pinheiro <joao@gtel.ufc.br>
2018-08-16 11:08:26 +0000

    Continued implementation of the resource manger.
    Added functions to create, start and stop domains.
    Added an example of domain XML definition
    Added script to install GNURadio on a virtual machine

--- code/resourcemanager/mainfunctions.py
+++ code/resourcemanager/mainfunctions.py
@@ -11,6 +11,7 @@ class VirtInstance:
             print(e)
             exit(1)
         self.uri = uri
+        self.domains = self.get_dom_dict()
 
     @staticmethod
     def connect(uri=LIBVIRT_URI):
@@ -26,27 +27,68 @@ class VirtInstance:
             raise Exception("Could not open connection to the HYPERVISOR")
         return conn
 
-    def list_domains(self):
-        """ Returns a dictionary in the format NAME:ID of all domains of the HYPERVISOR
+    def get_dom_dict(self):
+        """ Returns a dictionary of domains of the Hypervisor
+
+        The dictionary will be in the format NAME:ID
 
         :return: Dictionary of Names and IDs for the domains
         """
-        allDomains = self.conn.listAllDomains()
-        domDict = {}
-        for dom in allDomains:
-            domDict[dom.name()] = dom.ID()
-        return domDict
+        all_domains = self.conn.listAllDomains()
+        dom_dict = {}
+        for dom in all_domains:
+            dom_dict[dom.name()] = dom.ID()
+        return dom_dict
 
-    def create_domain(self):
-        raise NotImplementedError("This function was not implemented.")
+    def create_domain(self, dom_name=None, num_cpu=1, mem=1):
+        """
 
-    def start_domain(self, dom_name=None):
-        raise NotImplementedError("This function was not implemented.")
+        :param dom_name:
+        :param num_cpu:
+        :param mem:
+        :return:
+        """
+        with open("xmlExample.xml", "r") as f:
+            default_xml = f.read()
 
-    def stop_domain(self, dom_name=None):
-        raise NotImplementedError("This function was not implemented.")
+        default_xml = default_xml.replace("{NAME}", dom_name)
+        default_xml = default_xml.replace("{CPU}", str(num_cpu))
+        default_xml = default_xml.replace("{MEMORY}", str(mem))
 
-    def destroy_domain(self, dom_name=None):
+        domain = self.conn.defineXML(default_xml)
+        return domain
+
+    def start_domain(self, dom_name=None, domain=None):
+        """
+
+        :param dom_name:
+        :param domain:
+        :return:
+        """
+        if (not domain) and (dom_name is not None):
+            domain = self.conn.lookupByName(dom_name)
+
+        if domain.create() < 0:
+            raise Exception("The domain cold not be started.")
+        return domain
+
+    def stop_domain(self, dom_name=None, force_in_error=False, domain=None):
+        """
+
+        :param dom_name:
+        :param force_in_error:
+        :param domain:
+        :return:
+        """
+        if (not domain) and (dom_name is not None):
+            domain = self.conn.lookupByName(dom_name)
+
+        if (domain.shutdown() < 0) and (force_in_error is True):
+            domain.destroy()
+            return 1
+        return 0
+
+    def delete_domain(self, dom_name=None):
         raise NotImplementedError("This function was not implemented.")
 
     def clone_domain(self, src_dom_name=None, dst_dom_name=None):
@@ -57,6 +99,7 @@ if __name__ == '__main__':
     # This main function is for testing purposes only.
     # Will be removed once all functions are tested.
     c = VirtInstance()
-    print(c.list_domains())
-    print(c.start_domain())
+    # c.create_domain("tst")
+    # c.start_domain("tst")
+    c.stop_domain("tst")
     c.conn.close()
---
+++ code/scripts/install_vm.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ $USER != "root" ]; then
+    echo "Please run this script as root!"
+    exit 1
+fi
+
+
+#install Xorg
+sudo apt-get -y install xorg openbox
+
+# Install gnuradio
+sudo apt-get -y install git cmake g++ python-dev swig libvolk1-dev \
+pkg-config libfftw3-dev libboost-all-dev libcppunit-dev libgsl-dev \
+libusb-dev libsdl1.2-dev python-wxgtk3.0 python-numpy python-cheetah \
+python-lxml doxygen libxi-dev python-sip libqt4-opengl-dev libqwt-dev \
+libfontconfig1-dev libxrender-dev python-sip python-sip-dev python-qt4 \
+python-sphinx libusb-1.0-0-dev libcomedi-dev libzmq3-dev python-mako \
+python-gtk2-dev libuhd-dev uhd-host
+
+volk_profile
+
+git clone --recursive http://gnuradio.org/git/gnuradio.git
+
+cd gnuradio
+mkdir build
+cd build
+cmake ../
+make
+sudo make install
+
+export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python2.7/dist-packages
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+
+sudo ldconfig
+
+sudo addgroup usrp
+sudo usermod -aG usrp $USER 
+echo 'ACTION=="add", BUS=="usb", SYSFS{idVendor}=="fffe", 
+SYSFS{idProduct}=="0002", GROUP:="usrp", MODE:="0660"' > tmpfile
+sudo chown root.root tmpfile
+sudo mv tmpfile /etc/udev/rules.d/10-usrp.rules
+
+sudo /etc/init.d/udev stop
+sudo /etc/init.d/udev start
